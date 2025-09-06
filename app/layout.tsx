08ef6249cba5b6a805f49de847f88de81eb6cf20
AhnHyesook <samaishit@naver.com>
2025-09-06 15:03:28 +0900

    feat: API 기반 데이터 로딩으로 변환 완료

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,39 @@
-import type React from "react"
-import "./globals.css"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import { ThemeProvider } from "@/components/theme-provider"
+import type React from "react";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Mindful Blog",
   description: "창의적인 생각과 유용한 정보를 공유하는 공간",
-    generator: 'v0.dev'
-}
+  generator: "v0.dev",
+  verification: {
+    other: {
+      "naver-site-verification": "a75dcaccaf5d393364f274a93c8f73996566b419",
+    },
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="ko" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
